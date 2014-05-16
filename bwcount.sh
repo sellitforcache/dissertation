a34edec2dfc2a7cb2f2526a48e47e21f037410ba
Ryan M. Bergmann <rmb@berkeley.edu>
2014-05-16 00:44:40 -0700

    adding page color count script

--- bwcount.sh
+++ bwcount.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 bcount=`gs -o - -sDEVICE=inkcov  $1 | grep '0.00000  0.00000  0.00000' | wc -l`
-tcount=``
-echo 'BW page = $bcount, total pages=$tcount'
+tcount=`gs -o - -sDEVICE=inkcov  $1 | wc -l`
+declare -i bcount
+declare -i tcount
+tcount=(tcount-4)/2
+ccount=$((tcount-$bcount))
+echo "total pages=$tcount | BW pages = $bcount, COLOR pages = $ccount"
